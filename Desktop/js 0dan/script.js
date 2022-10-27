f4b7e27c57554b9dde1bced4f210bf8429674026
QodirovDoston <qodirovdoston806@gmail.com>
2022-10-27 18:00:24 +0300

    doston

---
+++ Desktop/js 0dan/script.js
@@ -0,0 +1,34 @@
+
+
+// document.getElementById("count-el").innerText = 5 
+
+// var a = 1
+// var b = 0
+// while (a <= 3){
+//     a++;
+//     b += a *2;
+//     print(b);
+// }
+// console.log(b);
+
+// var  a = [1,2,3,4,5,6]
+// console.log(a.slice(6));
+
+
+  // toq juftni aniqlash
+// var a = 43
+// a % 2 == 0 ?console.log( "juft"):console.log("toq");
+ 
+
+let obj = {
+  name:"doston"
+  ,age:32
+}
+console.log(obj.age + "MEN " + obj.name);
+
+// let a = +prompt("son kirgaz")
+// let b = +prompt("son kirgaz")
+// function wile ( a , b){
+// }
+// console.log(wile());
+// wile( a * b)
